3a835d9554974fcb06562899f91d96be556718f3
IDKOS1 <kss3736@naver.com>
2023-07-03 20:00:22 +0900

    seedColor blue -> deepPurple 변경

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'Screen/home_screen.dart';
 import 'Screen/bottom_navigator_bar.dart';
 
 void main() {
@@ -9,7 +8,7 @@ void main() {
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
             colorScheme: ColorScheme.fromSeed(
-                seedColor: Colors.blue,
+                seedColor: Colors.deepPurple,
                 brightness: Brightness.light,
             ),
           useMaterial3: true
