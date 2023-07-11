3828f547bc6dfdd445034596414fc4c91582006e
IDKOS1 <kss3736@naver.com>
2023-07-11 23:33:49 +0900

    firebase 라이브러리 추가

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,6 +7,7 @@ import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
 
+
 class LocationScreen extends StatefulWidget {
   @override
   _LocationChooserState createState() => _LocationChooserState();
